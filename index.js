f91487add8de51dbf30cc0465d49f1aeb154a773
Anthony Rurup <tcrurup@gmail.com>
2019-10-01 17:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 
-const append = ['Milo', 'Otis', 'Garfield']
-const prepent
-const removeLast
-const removeFIrst
+const append = ['Milo', 'Otis', 'Garfield'];
+const prepent = ['Milo', 'Otis', 'Garfield'];
+const removeLast = ['Milo', 'Otis', 'Garfield'];
+const removeFIrst = ['Milo', 'Otis', 'Garfield'];
